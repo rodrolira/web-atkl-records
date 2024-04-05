26b844801dd520513ec7be54d4fe1b9f97041f86
rodrolira <rlirapizarro@gmail.com>
2024-04-05 01:36:24 -0300

    Updates

--- assets/Artists-DOqajiGt.js
+++
@@ -1,7 +0,0 @@
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["assets/ArtistCard-ByR8qba5.js","assets/main-tApMXKhd.js","assets/main-CGJl1YdZ.css"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
-import{r as s,_ as i,u as t,j as a}from"./main-tApMXKhd.js";import{T as n}from"./Title-DxzOoQq-.js";const r=s.lazy(()=>i(()=>import("./ArtistCard-ByR8qba5.js"),__vite__mapDeps([0,1,2])));function l(){const{language:e}=t();return a.jsxs("div",{className:"m-32 inline-block",id:"artists",children:[a.jsx(n,{children:e==="en"?"Artists":"Artistas"}),a.jsx("div",{className:"grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4",children:a.jsxs(s.Suspense,{fallback:a.jsx("div",{children:"Loading..."}),children:[a.jsx(r,{id:1,name:"RODRO",role:"DJ",image:"/img/avatar.jpg"}),a.jsx(r,{id:2,name:"Sweet_Hate",role:"DJ",image:"/img/avatar.jpg"}),a.jsx(r,{id:3,name:"Abstracto",role:e==="en"?"Producer":"Productor",image:"/img/avatar.jpg"}),a.jsx(r,{id:4,name:"DARKNOISE",role:e==="en"?"Producer":"Productor",image:"/img/avatar.jpg"})]})})]})}export{l as default};
