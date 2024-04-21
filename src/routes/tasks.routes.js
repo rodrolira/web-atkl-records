4b738ca81890809cbfaddcc87e63d32915bf491f
rodrolira <rlirapizarro@gmail.com>
2024-04-20 23:38:35 -0400

    Add Zod for input validation
    - Add Zod as a dependency
    - Add validation for auth routes (register and login) using Zod
    - Add validation for tasks route (createTask) using Zod
    - Update package-lock

--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -1,12 +1,14 @@
 import { Router } from 'express';
 import { authRequired } from "../middlewares/validateToken.js";
 import { getTasks, createTask, getTask, updateTask, deleteTask } from '../controllers/tasks.controller.js';
+import { validateSchema } from '../middlewares/validator.middleware.js';
+import { createTaskSchema } from '../schemas/task.schema.js';
 
 const router = Router();
 
 router.get('/tasks', authRequired, getTasks)
 router.get('/tasks/:id', authRequired, getTask)
-router.post('/tasks', authRequired, createTask)
+router.post('/tasks', authRequired, validateSchema(createTaskSchema), createTask)
 router.delete('/tasks/:id', authRequired, deleteTask)
 router.put('/tasks/:id', authRequired, updateTask)
 
