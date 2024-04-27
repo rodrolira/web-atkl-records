57d8731869caea08e2041e8069a04c20da014ea8
rodrolira <rlirapizarro@gmail.com>
2024-04-27 04:10:27 -0400

    Update authentication middleware and context
    - Add adminAuthRequired middleware for admin route protection
    - Update AuthContext to include loading state and error handling
    - Add loginRequest function to AuthContext for user login
    - Add verityTokenRequest

--- client/src/contexts/AuthContext.jsx
+++ client/src/contexts/AuthContext.jsx
@@ -1,22 +1,22 @@
-import { createContext, useContext, useState } from 'react'
-import { registerRequest } from '../api/auth'
+import { createContext, useContext, useEffect, useState } from 'react'
+import { registerRequest, loginRequest, verityTokenRequest } from '../api/auth'
+import Cookies from 'js-cookie'
 
 export const AuthContext = createContext()
 
 export const useAuth = () => {
   const context = useContext(AuthContext)
-
   if (!context) {
     throw new Error('useAuth must be used within an AuthProvider')
   }
-
   return context
 }
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null)
   const [isAuthenticated, setIsAuthenticated] = useState(false)
-  const [errors, setErrors] = useState(null)
+  const [errors, setErrors] = useState([])
+  const [loading, setLoading] = useState(true)
 
   const signup = async user => {
     try {
@@ -29,10 +29,67 @@ export const AuthProvider = ({ children }) => {
       setErrors(error.response.data)
     }
   }
+
+  const signin = async user => {
+    try {
+      const res = await loginRequest(user)
+      console.log(res)
+      setIsAuthenticated(true)
+      setUser(res.data)
+    } catch (error) {
+      if (Array.isArray(error.response.data)) {
+        return setErrors(error.response.data)
+      }
+
+      setErrors([error.response.data.message])
+    }
+  }
+
+  useEffect(() => {
+    if (errors.length > 0) {
+      const timer = setTimeout(() => {
+        setErrors([])
+      }, 5000)
+      return () => clearTimeout(timer)
+    }
+  }, [errors])
+
+  useEffect(() => {
+    async function checkLogin () {
+      const cookies = Cookies.get()
+
+      if (!cookies.token) {
+        setIsAuthenticated(false)
+        setLoading(false)
+        return setUser(null)
+      }
+
+      try {
+        const res = await verityTokenRequest(cookies.token)
+        if (!res.data) {
+          setIsAuthenticated(false)
+          setLoading(false)
+          return
+        }
+
+        setIsAuthenticated(true)
+        setUser(res.data)
+        setLoading(false)
+      } catch (error) {
+        setIsAuthenticated(false)
+        setUser(null)
+        setLoading(false)
+      }
+    }
+    checkLogin()
+  }, [])
+
   return (
     <AuthContext.Provider
       value={{
         signup,
+        signin,
+        loading,
         user,
         isAuthenticated,
         errors
