ed72cf8358f5b13aaff1243a3fea377f67293a18
rodrolira <rlirapizarro@gmail.com>
2024-05-07 22:58:28 -0400

    a

--- client/src/contexts/ArtistContext.jsx
+++ client/src/contexts/ArtistContext.jsx
@@ -1,5 +1,5 @@
 import { createContext, useContext, useState } from 'react'
-// import { createArtistRequest } from '../api/artists'
+import { createArtistRequest } from '../api/artists'
 const ArtistContext = createContext()
 
 export const useArtists = () => {
@@ -14,10 +14,10 @@ export const useArtists = () => {
 export function ArtistProvider ({ children }) {
   const [artists, setArtists] = useState([]) // Estado para almacenar la lista de artistas
 
-//   const createArtist = async artist => {
-//   const res=  await createArtistRequest(artist)
-//     console.log(res)
-// }
+   const createArtist = async artist => {
+   const res=  await createArtistRequest(artist)
+     console.log(res)
+ }
 
   return (
     <ArtistContext.Provider value={{ artists }}>
