499403984077ae0758a7e91d201ab59ad960935b
rodrolira <rlirapizarro@gmail.com>
2024-05-18 00:07:32 -0400

    "Updated dependencies and code in client package"

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,9 @@ export default {
         'node_modules/flowbite-react/lib/esm/**/*.js',
         './node_modules/tw-elements-react/dist/js/**/*.js',
         './node_modules/flowbite/**/*.js',
+        './node_modules/flowbite-react/**/*.js',
+        './node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}',
+        './node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}',
     ],
     theme: {
         extend: {},
