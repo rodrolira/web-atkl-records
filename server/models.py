5a146e162f0a00b0e2fac532b566480b8869f45a
rodrolira <rlirapizarro@gmail.com>
2024-05-28 21:33:22 -0400

    a

--- server/models.py
+++
@@ -1,8 +0,0 @@
-from database import db
-
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50))
-    email = db.Column(db.String(100), unique=True)
-    password = db.Column(db.String(255))
