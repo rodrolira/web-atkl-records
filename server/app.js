bec27fbde0f117ee98874b9154daffcad0511f29
rodrolira <rlirapizarro@gmail.com>
2024-06-28 19:04:00 -0400

    "Updated CORS configuration in app.js"

--- server/app.js
+++ server/app.js
@@ -11,31 +11,31 @@ import releasesRoutes from "./routes/releases.routes.js";
 import adminRoutes from "./routes/admin.routes.js";
 import contactFormRoutes from "./routes/contact-form.routes.js";
 
-
 dotenv.config();
 
 const app = express();
+
 // Middleware para servir archivos estáticos
 app.use("/uploads", express.static("uploads"));
 
- app.use(
-   cors({
-     origin: 'https://atkl-react2-fzwl.vercel.app',
-     credentials: true,
-     exposedHeaders: 'Access-Control-Allow-Origin' // Agrega esta línea
-   })
- )
 // app.use(
 //   cors({
-//     origin: "http://localhost:5173",
+//     origin: 'https://atkl-react2-fzwl.vercel.app',
 //     credentials: true,
-//     methods: ["GET", "POST", "DELETE"], // Agrega DELETE aquí si es necesario
+//     exposedHeaders: 'Access-Control-Allow-Origin' // Agrega esta línea
 //   })
-// );
+// )
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "DELETE", "PUT"], // Agrega DELETE aquí si es necesario
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cookieParser());
-
 
 // Rutas públicas
 app.use("/api", authRoutes);
@@ -53,26 +53,9 @@ app.use((err, req, res, next) => {
   next();
 });
 
-
-
-// Ruta para manejar POST requests a /api/releases
-app.post("/api/releases", (req, res) => {
-  const newRelease = req.body;
-
-  // Aquí deberías agregar la lógica para guardar el nuevo lanzamiento
-  // Ejemplo: Guardar en la base de datos
-
-  res
-    .status(201)
-    .json({ message: "Release created successfully", release: newRelease });
-});
-
 // Middleware para manejo de rutas no encontradas
 app.use((req, res) => {
   res.status(404).json({ error: "Page not found" });
 });
-
-
-
 
 export default app;
