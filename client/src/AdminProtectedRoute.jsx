e796a4fdf9a76af62f841768b729d3bd2c5724bf
rodrolira <rlirapizarro@gmail.com>
2024-06-30 04:09:31 -0400

    "Refactored AdminProtectedRoute and App components, removed unnecessary imports and comments, and updated route configurations."

--- client/src/AdminProtectedRoute.jsx
+++ client/src/AdminProtectedRoute.jsx
@@ -1,20 +1,17 @@
 // // components/AdminProtectedRoute.jsx
 
-// import React from 'react'
-// import { Navigate, Outlet } from 'react-router-dom'
-// import { useAdminAuth } from './contexts/AdminAuthContext'
+import React from 'react'
+import { Navigate, Outlet } from 'react-router-dom'
+import { useAdminAuth } from './contexts/AdminAuthContext'
 
-// // Ruta protegida para administradores
-// const AdminProtectedRoute = () => {
-//     const { isAuthenticated } = useAdminAuth()
-
-//     if (!isAuthenticated) {
-//         // Redirigir a la página de login si no está autenticado
-//         return <Navigate to="/admin/login" replace />
-//     }
-
-//     // Renderizar el contenido protegido si está autenticado
-//     return <Outlet />
-// }
-
-// export default AdminProtectedRoute
+// Ruta protegida para administradores
+const AdminProtectedRoute = () => {
+    const { isAuthenticated } = useAdminAuth()
+    if (!isAuthenticated) {
+        // Redirigir a la página de login si no está autenticado
+        return <Navigate to='/login' replace />
+    }
+    // Renderizar el contenido protegido si está autenticado
+    return <Outlet />
+}
+export default AdminProtectedRoute
