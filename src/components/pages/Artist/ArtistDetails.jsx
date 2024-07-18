59b3e8f02e94339c34616e278027d2d8d9262928
rodrolira <rlirapizarro@gmail.com>
2024-07-17 21:23:56 -0400

    "Refactor ArtistDetails, ArtistImage, EditArtistModal, ArtistContext, and useArtists hook components"

--- src/components/pages/Artist/ArtistDetails.jsx
+++ src/components/pages/Artist/ArtistDetails.jsx
@@ -12,11 +12,13 @@ const ArtistDetails = ({ artist, adminAuthenticated, openEditModal }) => {
         <div className='w-1/3 p-4 border-r text-center text-white'>
             <ArtistName name={artist.artist_name} adminAuthenticated={adminAuthenticated} openEditModal={openEditModal} />
             <ArtistImage image={`http://localhost:3000/${artist.image}`} alt={artist.artist_name} />
-            <div className='bg-slate-900 border-gray-200 w-full h-full relative rounded-b-lg'>
-                <h1 className='text-2xl font-semibold tracking-tight text-white text-center'>
-                    {artist.role}
-                </h1>
-                <ArtistLinks artist={artist} />
+            <div className='px-4'>
+                <div className='bg-slate-900 border-gray-200 w-full h-auto relative rounded-b-lg'>
+                    <h1 className='text-2xl font-semibold tracking-tight text-white text-center'>
+                        {artist.role}
+                    </h1>
+                    <ArtistLinks artist={artist} />
+                </div>
             </div>
         </div>
     );
