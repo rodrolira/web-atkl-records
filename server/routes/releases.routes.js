0aa3b22f290f2d1fc36935031965f2b12bdf481a
rodrolira <rlirapizarro@gmail.com>
2024-07-20 18:37:04 -0400

    Refactor API and components for releases, genres, and artists; update controllers and routes

--- server/routes/releases.routes.js
+++ server/routes/releases.routes.js
@@ -1,48 +1,48 @@
 // routes/release.routes.js
-import express from "express";
-const router = express.Router();
-import multer from "multer"; // Importa multer para manejar la carga de archivos
+import express from 'express'
+import multer from 'multer' // Importa multer para manejar la carga de archivos
 import {
   addRelease,
   getReleases,
   getReleaseById,
   updateRelease,
   deleteRelease,
-} from "../controllers/releases.controller.js";
+} from '../controllers/releases.controller.js'
+const router = express.Router()
 
 // Configuración de multer
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "uploads/"); // Define dónde se almacenarán los archivos
+    cb(null, 'uploads/') // Define dónde se almacenarán los archivos
   },
   filename: (req, file, cb) => {
-    cb(null, `${Date.now()}_${file.originalname}`); // Define el nombre del archivo
+    cb(null, `${Date.now()}_${file.originalname}`) // Define el nombre del archivo
   },
-});
+})
 
 const upload = multer({
   storage,
   limits: { fileSize: 1024 * 1024 * 5 }, // Example limit: 5MB
   fileFilter: (req, file, cb) => {
     if (
-      file.mimetype === "image/jpeg" ||
-      file.mimetype === "image/png" ||
-      file.mimetype === "image/jpg"
+      file.mimetype === 'image/jpeg' ||
+      file.mimetype === 'image/png' ||
+      file.mimetype === 'image/jpg'
     ) {
-      cb(null, true);
+      cb(null, true)
     } else {
-      cb(new Error("Invalid file type"), false);
+      cb(new Error('Invalid file type'), false)
     }
   },
-});
+})
 
 // Rutas
-router.get("/releases", getReleases);
-router.get("/releases/:id", getReleaseById);
+router.get('/releases', getReleases)
+router.get('/releases/:id', getReleaseById)
 
-router.post("/releases", upload.single("cover_image_url"), addRelease);
+router.post('/releases', upload.single('cover_image_url'), addRelease)
 
-router.put("/releases/:id", upload.single("cover_image_url"), updateRelease);
-router.delete("/releases/:id", deleteRelease);
+router.put('/releases/:id', upload.single('cover_image_url'), updateRelease)
+router.delete('/releases/:id', deleteRelease)
 
-export default router;
+export default router
