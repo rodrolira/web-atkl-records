694f3c64c4040e6ca1f7db8ccebe3d1301d5878a
rodrolira <rlirapizarro@gmail.com>
2024-07-20 22:23:57 -0400

    "Updated code formatting and syntax in releases.controller.js and artists.routes.js files, including changes to quotes, semicolons, and indentation."

--- server/routes/artists.routes.js
+++ server/routes/artists.routes.js
@@ -1,6 +1,5 @@
 // routes/artists.routes.js
-import express from "express";
-const router = express.Router();
+import express from 'express'
 import {
   getArtists,
   addArtist,
@@ -8,28 +7,29 @@ import {
   updateArtist,
   getArtistById,
   getArtistReleases,
-} from "../controllers/artists.controller.js";
-import multer from "multer";
+} from '../controllers/artists.controller.js'
+import multer from 'multer'
+const router = express.Router()
 
 // Configuración de multer
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "uploads/"); // Define dónde se almacenarán los archivos
+    cb(null, 'uploads/') // Define dónde se almacenarán los archivos
   },
   filename: (req, file, cb) => {
-    cb(null, `${Date.now()}_${file.originalname}`); // Define el nombre del archivo
+    cb(null, `${Date.now()}_${file.originalname}`) // Define el nombre del archivo
   },
-});
+})
 
-const upload = multer({ storage });
+const upload = multer({ storage })
 
-router.get("/artists", getArtists);
-router.get("/artists/:id", getArtistById);
-router.get("/artists/:id/releases", getArtistReleases);
+router.get('/artists', getArtists)
+router.get('/artists/:id', getArtistById)
+router.get('/artists/:id/:releaseId', getArtistReleases)
 
-router.post("/artists", upload.single("image"), addArtist);
+router.post('/artists', upload.single('image'), addArtist)
 
-router.put("/artists/:id", upload.single("image"), updateArtist);
-router.delete("/artists/:id", deleteArtist);
+router.put('/artists/:id', upload.single('image'), updateArtist)
+router.delete('/artists/:id', deleteArtist)
 
-export default router;
+export default router
