b244e8df5c86d8a02fde3bae5e66f79cdd518db0
rodrolira <rlirapizarro@gmail.com>
2024-07-21 02:26:46 -0400

    "Refactored auth controller and routes to improve code readability and consistency"

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,109 +1,106 @@
 // user.controller.js
 
-import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
-import User from "../models/user.model.js";
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+import User from '../models/user.model.js'
 
 const createToken = (userId) => {
   return jwt.sign({ userId }, process.env.SECRET, {
-    expiresIn: "12h",
-  });
-};
+    expiresIn: '12h',
+  })
+}
 
 export const createUser = async ({ username, email, password }) => {
-  const hashedPassword = await bcrypt.hash(password, 10);
+  const hashedPassword = await bcrypt.hash(password, 10)
 
   try {
     const newUser = await User.create({
       username,
       email,
       password: hashedPassword,
-    });
-    return newUser;
+    })
+    return newUser
   } catch (error) {
-    throw new Error(`Error creating user: ${error.message}`);
+    throw new Error(`Error creating user: ${error.message}`)
   }
-};
+}
 
 export const findUserByEmail = async (email) => {
   try {
-    const user = await User.findOne({ where: { email } });
-    return user;
+    const user = await User.findOne({ where: { email } })
+    return user
   } catch (error) {
-    throw new Error(`Error finding user by email: ${error.message}`);
+    throw new Error(`Error finding user by email: ${error.message}`)
   }
-};
+}
 
 export const findUserByUsername = async (username) => {
   try {
-    const user = await User.findOne({ where: { username } });
-    return user;
+    const user = await User.findOne({ where: { username } })
+    return user
   } catch (error) {
-    throw new Error(`Error finding user by username: ${error.message}`);
+    throw new Error(`Error finding user by username: ${error.message}`)
   }
-};
+}
 
 export const loginUser = async (username, password) => {
   try {
-    const user = await User.findOne({ where: { username } });
+    const user = await User.findOne({ where: { username } })
 
     if (!user) {
-      throw new Error("User not found");
+      throw new Error('User not found')
     }
 
-    const isMatch = await bcrypt.compare(password, user.password);
+    const isMatch = await bcrypt.compare(password, user.password)
 
     if (!isMatch) {
-      throw new Error("Invalid credentials");
+      throw new Error('Invalid credentials')
     }
 
     const token = createToken(user.id)
 
-    return token;
+    return token
   } catch (error) {
-    throw new Error(`Error logging in user: ${error.message}`);
+    throw new Error(`Error logging in user: ${error.message}`)
   }
-};
-
+}
 
 export const profileUser = async (userId) => {
   try {
-    const user = await User.findByPk(userId);
-
+    const user = await User.findByPk(userId, { attributes: ['id', 'username', 'email'] })
     if (!user) {
-      throw new Error("User not found");
+      throw new Error('User not found')
     }
-
-    return user;
+    return user
   } catch (error) {
-    throw new Error(`Error fetching user profile: ${error.message}`);
+    throw new Error(`Error fetching user profile: ${error.message}`)
   }
-};
+}
 
 export const verifyTokenUser = async (token) => {
   try {
-    const decoded = jwt.verify(token, process.env.SECRET);
-    const user = await User.findByPk(decoded.userId);
+    const decoded = jwt.verify(token, process.env.SECRET)
+    const user = await User.findByPk(decoded.userId)
 
     if (!user) {
-      throw new Error("User not found");
+      throw new Error('User not found')
     }
 
-    return user;
+    return user
   } catch (error) {
-    if (error.name === "TokenExpiredError") {
-      throw new Error("Token expired");
+    if (error.name === 'TokenExpiredError') {
+      throw new Error('Token expired')
     } else {
-      throw new Error(`Error verifying user token: ${error.message}`);
+      throw new Error(`Error verifying user token: ${error.message}`)
     }
   }
-};
+}
 
 export const logoutUser = (req, res) => {
   try {
-    res.clearCookie("token");
-    res.status(200).json({ message: "Logged out successfully" });
+    res.clearCookie('token')
+    res.status(200).json({ message: 'Logged out successfully' })
   } catch (error) {
-    res.status(500).json({ message: `Error logging out: ${error.message}` });
+    res.status(500).json({ message: `Error logging out: ${error.message}` })
   }
-};
+}
--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,71 +1,71 @@
 // user.routes.js
 
-import express from "express";
-import * as userController from "../controllers/auth.controller.js";
-import { verifyTokenUser } from "../middlewares/auth.middleware.js";
+import express from 'express'
+import * as userController from '../controllers/auth.controller.js'
+import { verifyTokenUser } from '../middlewares/auth.middleware.js'
 
-const router = express.Router();
+const router = express.Router()
 
-router.post("/register", async (req, res) => {
+router.post('/register', async (req, res) => {
   try {
-    const { username, email, password } = req.body;
+    const { username, email, password } = req.body
     const newUser = await userController.createUser({
       username,
       email,
       password,
-    });
+    })
     res
       .status(201)
-      .json({ message: "User registered successfully", user: newUser });
+      .json({ message: 'User registered successfully', user: newUser })
   } catch (error) {
-    console.error("Error registering user:", error);
-    res.status(500).json({ message: "Server Error" });
+    console.error('Error registering user:', error)
+    res.status(500).json({ message: 'Server Error' })
   }
-});
+})
 
-router.post("/login", async (req, res) => {
+router.post('/login', async (req, res) => {
   try {
-    const { username, password } = req.body;
-    const token = await userController.loginUser(username, password);
+    const { username, password } = req.body
+    const token = await userController.loginUser(username, password)
 
-    res.cookie("token", token, {
+    res.cookie('token', token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "dev",
+      secure: process.env.NODE_ENV === 'dev',
       maxAge: 12 * 60 * 60 * 1000, // 12hrs
-    });
+    })
 
-    res.json({ message: "Login successful", user: { username, password } });
+    res.json({ message: 'Login successful', user: { username, password } })
   } catch (error) {
-    console.error("Error logging in user:", error);
-    res.status(401).json({ message: "Invalid credentials" });
+    console.error('Error logging in user:', error)
+    res.status(401).json({ message: 'Invalid credentials' })
   }
-});
+})
 
-router.post("/logout", userController.logoutUser);
+router.post('/logout', userController.logoutUser)
 
-router.get("/profile", verifyTokenUser, async (req, res) => {
+router.get('/profile', verifyTokenUser, async (req, res) => {
   try {
-    const user = await userController.profileUser(req.userId);
-    res.json({ user });
+    const user = await userController.profileUser(req.userId)
+    res.json({ user })
   } catch (error) {
-    console.error("Error fetching user profile:", error);
-    res.status(500).json({ message: "Server Error" });
+    console.error('Error fetching user profile:', error)
+    res.status(500).json({ message: 'Server Error' })
   }
-});
+})
 
-router.get("/verify", async (req, res) => {
-  const { token } = req.cookies;
+router.get('/verify', async (req, res) => {
+  const { token } = req.cookies
   try {
-    const user = await userController.verifyTokenUser(token);
-    res.json({ user });
+    const user = await userController.verifyTokenUser(token)
+    res.json({ user })
   } catch (error) {
-    if (error.message === "Token expired") {
-      res.status(401).json({ message: "Token expired" });
+    if (error.message === 'Token expired') {
+      res.status(401).json({ message: 'Token expired' })
     } else {
-      console.error("Error verifying user token:", error);
-      res.status(401).json({ message: "Unauthorized" });
+      console.error('Error verifying user token:', error)
+      res.status(401).json({ message: 'Unauthorized' })
     }
   }
-});
+})
 
-export default router;
+export default router
