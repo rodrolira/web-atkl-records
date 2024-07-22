ae7a1ccb52a251d3cfd8233292f4b7824711b04e
rodrolira <rlirapizarro@gmail.com>
2024-07-22 12:21:51 -0400

    feat: Add AddTrackForm component to DiscographyPage
    This commit adds the AddTrackForm component to the DiscographyPage. The form allows users to upload tracks in CSV format, which are then processed and added to the discography. This feature enhances the functionality of the DiscographyPage by enabling users to easily add multiple tracks at once.
    
    Resolves: #456

--- client/src/components/pages/Discography/DiscographyPage.jsx
+++ client/src/components/pages/Discography/DiscographyPage.jsx
@@ -1,23 +1,19 @@
 import React from 'react'
 import TrackList from './TrackList'
+import AddTrackForm from './AddTrackForm'
 import Navbar from '../../organisms/Navbar'
+import Title from '../../atoms/Title'
 
-const DiscographyPage = () => {
-  // Aquí puedes obtener la lista de canciones de tu sello musical
-  const discography = [
-    { title: 'Canción 1', artist: 'Artista 1', download_url: 'https://example.com/download/1' },
-    // Agrega más canciones aquí
-  ]
-
+const DiscographyPage = ({ children }) => {
   return (
     <>
       <Navbar />
       <div className='mt-32'>
-        <h1>Discografía</h1>
-        <TrackList discography={discography} />
+        <Title>Discography</Title>
+        <AddTrackForm />
+        <TrackList />
       </div>
     </>
-
   )
 }
 
