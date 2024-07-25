e8a3bc91e5d19f549e65a94b4c4a5cdd26ddd68b
rodrolira <rlirapizarro@gmail.com>
2024-07-25 02:20:18 -0400

    Refactor Layout component to remove unused imports and optimize code
    This commit refactors the Layout component to remove unused imports and optimize the code. The unnecessary imports for ReleaseProvider, ArtistProvider, LanguageProvider, and GenreProvider are removed. This improves the code readability and reduces unnecessary dependencies. The commented out import for AdminAuthProvider is also removed. These changes enhance the maintainability and performance of the Layout component.
    
    Resolves: #N/A

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,12 +1,7 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
-import { ReleaseProvider } from '../contexts/ReleaseContext'
-import { ArtistProvider } from '../contexts/ArtistContext'
-import { LanguageProvider } from '../contexts/LanguageContext'
 import Footer from '../components/organisms/Footer'
 import '../App.css'
-import { GenreProvider } from '../contexts/GenreContext'
-// import AdminAuthProvider from '../contexts/AdminAuthContext'
 
 const Layout = ({ children }) => {
     return (
