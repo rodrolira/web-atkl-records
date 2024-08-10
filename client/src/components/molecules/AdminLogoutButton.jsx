a3473bc2d8be41e29c07e4ad220ed8c92cd576c3
rodrolira <rlirapiarro@gmail.com>
2024-08-09 22:20:43 -0400

    refactor: Update AdminLogoutButton component to use react-i18next for translation

--- client/src/components/molecules/AdminLogoutButton.jsx
+++ client/src/components/molecules/AdminLogoutButton.jsx
@@ -3,13 +3,13 @@
 import React from 'react'
 import Button from '../atoms/Button'
 import { useNavigate } from 'react-router-dom'
-import { useLanguage } from '../../contexts/LanguageContext'
 import { useAdminAuth } from '../../contexts/AdminAuthContext'
+import { useTranslation } from 'react-i18next'
 
 const AdminLogoutButton = () => {
-    const { language } = useLanguage()
     const { signout } = useAdminAuth()
     const navigate = useNavigate()
+    const { t } = useTranslation()
 
     const handleLogout = async () => {
         await signout()
@@ -22,7 +22,7 @@ const AdminLogoutButton = () => {
                 className="justify-center lg:text-sm md:text-xs sm:w-32 md:w-auto h-full md:font-normal lg:font-bold bg-red-600 hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 px-5 md:m-0 lg:mx-2 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800 md:order-2"
                 onClick={handleLogout}
             >
-                {language === 'en' ? 'Logout' : 'Salir'}
+                {t('logout')}
             </Button>
         </div>
     )
