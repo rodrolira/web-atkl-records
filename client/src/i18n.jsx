3d221bc523a72e15a24a2ff3d59b25933a6c2199
rodrolira <rlirapiarro@gmail.com>
2024-08-09 22:18:49 -0400

    feat: Update script source URL and i18next dependency version
    The script source URL in the index.html file has been updated to use port 8097 instead of 3000. This change ensures that the correct script is loaded for the development environment.
    
    Additionally, the i18next dependency in the package.json file has been updated to version 23.12.2. This update includes bug fixes and improvements to the internationalization functionality.
    
    Resolves: #N/A

--- client/src/i18n.jsx
+++ client/src/i18n.jsx
@@ -1,6 +1,48 @@
+import { Logout } from '@mui/icons-material'
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 
-i18n.use(initReactI18next)
+const resources = {
+  en: {
+    translation: {
+        english: 'English',
+        spanish: 'Spanish',
+        logout: 'Logout',
+        subscribe: 'Subscribe',
+        name: 'Name',
+        email: 'Email',
+        description: 'Description (Optional)',
+        submit: 'Submit',
+        subscribe_message: 'Subscribe to receive the latest releases for free by email',
+        description_optional: 'Description (Optional)',
+        form_sent_success: 'Form successfully submitted'
+
+    }
+  },
+  es: {
+    translation: {
+        english: 'Inglés',
+        spanish: 'Español',
+        logout: 'Cerrar sesión',
+        subscribe: 'Suscribirse',
+        name: 'Nombre',
+        email: 'Correo electrónico',
+        description: 'Descripción (Opcional)',
+        submit: 'Enviar',
+        subscribe_message: 'Suscribirse para recibir las últimas actualizaciones gratis por correo electrónico',
+        description_optional: 'Descripción (Opcional)',
+        form_sent_success: 'Formulario enviado con éxito'
+        // Agrega más traducciones aquí
+    }
+  }
+}
+
+i18n.use(initReactI18next).init({
+  resources,
+  lng: 'en', // idioma por defecto
+  interpolation: {
+    escapeValue: false // React ya se encarga de esto
+  }
+})
 
 export default i18n
