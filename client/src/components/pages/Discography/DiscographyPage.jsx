062dcc4fe6ece092c36572e937db02e8815a2f91
rodrolira <rlirapiarro@gmail.com>
2024-08-15 22:20:43 -0400

    feat: Update DiscographyPage to conditionally render AddTrackForm based on admin authentication
    This commit updates the DiscographyPage component to conditionally render the AddTrackForm component based on the admin authentication status. The isAuthenticated variable from the useAdminAuth hook is used to determine whether to display the form. This change ensures that only authenticated admins can add tracks to the discography.
    
    Resolves: #N/A

--- client/src/components/pages/Discography/DiscographyPage.jsx
+++ client/src/components/pages/Discography/DiscographyPage.jsx
@@ -3,14 +3,17 @@ import TrackList from './TrackList'
 import AddTrackForm from './AddTrackForm'
 import Navbar from '../../organisms/Navbar'
 import Title from '../../atoms/Title'
+import { useAdminAuth } from '../../../contexts/AdminAuthContext'
 
 const DiscographyPage = ({ children }) => {
+  const { isAuthenticated } = useAdminAuth() // Obtén el estado de autenticación del admin
+
   return (
     <>
       <Navbar />
       <div className='mt-32'>
         <Title>Discography</Title>
-        <AddTrackForm />
+        {isAuthenticated && <AddTrackForm />}
         <TrackList />
       </div>
     </>
