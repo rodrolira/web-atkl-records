fa677ab2df4a940c22641c1bbd07d1fb30f9bd14
rodrolira <rlirapiarro@gmail.com>
2024-09-01 23:34:50 -0400

    refactor: Update responsive styles in App.css, Navbar.jsx, and LoginButton.jsx
    This commit updates the responsive styles in the App.css, Navbar.jsx, and LoginButton.jsx files. The changes include adding media queries to adjust the layout and dimensions of elements for smaller screens. These updates improve the responsiveness and user experience of the application.
    
    Resolves: #N/A

--- src/components/organisms/Navbar.jsx
+++ src/components/organisms/Navbar.jsx
@@ -8,8 +8,8 @@ function Navbar() {
     const { user } = useAuth()
     return (
 <nav className='shadow-lg navbar-wrapper fixed top-0 w-full h-20 lg:h-24 z-10 bg-[#122e0f] !border-[#22581d]'>
-<div className='flex lg:flex-row h-full justify-between px-2 pt-2 md:pt-0 md:flex-col'>
-                <div className='my-auto h-full lg:ml-12'>
+<div className='lg:flex lg:flex-row h-full justify-between px-2 pt-2 md:pt-0 md:flex-col'>
+                <div className='my-auto h-full lg:ml-12 mx-auto'>
                     <NavbarLogo alt='Company Logo' />
                 </div>
                 <NavbarMenu userId={user ? user.userId : null} />
