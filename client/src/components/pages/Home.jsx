1c1a668620c9d1edec4c69e1cb6c84e013827fde
rodrolira <rlirapiarro@gmail.com>
2024-09-05 00:10:58 -0400

    feat: Add Spanish translations
    This commit introduces Spanish translations for the website, including UI elements, content, and form labels. This enhances accessibility and caters to a wider user base.

--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -28,44 +28,44 @@ function Home() {
         <>
             <Navbar />
             <div id="home" className="home">
-            <div className="w-full parallax-container flex relative h-screen overflow-hidden">
-                <div className="w-full absolute parallax-content inset-0 bg-gradient-to-b from-transparent to-[#122e0f] backdrop-blur-md">
-                    {/* Contenido del encabezado aquí */}
-                    <div className="container h-full">
-                        <section
-                            id="main"
-                            className="main h-full w-full flex flex-col justify-end"
-                        >
-                            <div className="logo-main flex items-center md:h-full w-full">
-                                <img
-                                    alt="main"
-                                    className="md:h-96 mt-0 mx-auto md:mt-80 h-36"
-                                    src="/img/main.png"
-                                />
-                            </div>
-                            <div className="text-center mx-auto w-full text-white flex flex-col lg:mb-24">
-                                <h1 className="font-extrabold mx-auto text-center lg:text-5xl text-xl">
-                                    {language === 'en'
-                                        ? 'HARD TECHNO IS LIFE'
-                                        : 'HARD TECHNO IS LIFE'}
-                                </h1>
-                                <h2 className="text-lg lg:text-3xl ">
-                                    {language === 'en'
-                                        ? 'LABEL'
-                                        : 'SELLO DISCOGRÁFICO'}
-                                </h2>
-                            </div>
-                        </section>
+                <div className="w-full parallax-container flex relative h-screen overflow-hidden">
+                    <div className="w-full absolute parallax-content inset-0 bg-gradient-to-b from-transparent to-[#122e0f] backdrop-blur-md">
+                        {/* Contenido del encabezado aquí */}
+                        <div className="container h-full">
+                            <section
+                                id="main"
+                                className="main h-full w-full flex flex-col justify-end"
+                            >
+                                <div className="logo-main flex items-center md:h-full w-full">
+                                    <img
+                                        alt="main"
+                                        className="md:h-96 mt-0 mx-auto md:mt-80 h-36"
+                                        src="/img/main.png"
+                                    />
+                                </div>
+                                <div className="text-center mx-auto w-full text-white flex flex-col lg:mb-24">
+                                    <h1 className="font-extrabold mx-auto text-center lg:text-5xl text-xl">
+                                        {language === 'en'
+                                            ? 'HARD TECHNO IS LIFE'
+                                            : 'HARD TECHNO IS LIFE'}
+                                    </h1>
+                                    <h2 className="text-lg lg:text-3xl ">
+                                        {language === 'en'
+                                            ? 'LABEL'
+                                            : 'SELLO DISCOGRÁFICO'}
+                                    </h2>
+                                </div>
+                            </section>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <ReleasesSection />
-            <ArtistsSection />
-            <AboutSection />
-            <DemosSection />
-            <ContactSection />
-        </div>
+                <ReleasesSection />
+                <ArtistsSection />
+                <AboutSection />
+                <DemosSection />
+                <ContactSection />
+            </div>
         </>
     )
 }
--- client/src/i18n.js
+++ client/src/i18n.js
@@ -102,114 +102,114 @@ i18n.use(initReactI18next).init({
         },
       },
     },
-      es: {
-        translation: {
-          english: 'Inglés',
-          spanish: 'Español',
-          logout: 'Cerrar sesión',
-          subscribe: 'Suscribirse',
-          name: 'Nombre',
+    es: {
+      translation: {
+        english: 'Inglés',
+        spanish: 'Español',
+        logout: 'Cerrar sesión',
+        subscribe: 'Suscribirse',
+        name: 'Nombre',
+        email: 'Correo electrónico',
+        description: 'Descripción (Opcional)',
+        submit: 'Enviar',
+        subscribe_message: 'Suscríbete para recibir las últimas actualizaciones gratis por correo electrónico',
+        description_optional: 'Descripción (Opcional)',
+        form_sent_success: 'Formulario enviado con éxito',
+        send_demo: 'Enviar Demo',
+        upload_cover_image: 'Subir Portada',
+        upload_profile_image: 'Subir Imagen de Perfil',
+        artistName: 'Nombre del Artista',
+        image: 'Imagen',
+        bandcamp_link: 'Enlace de Bandcamp',
+        facebook_link: 'Enlace de Facebook',
+        instagram_link: 'Enlace de Instagram',
+        soundcloud_link: 'Enlace de SoundCloud',
+        twitter_link: 'Enlace de Twitter',
+        youtube_link: 'Enlace de YouTube',
+        spotify_link: 'Enlace de Spotify',
+        beatport_link: 'Enlace de Beatport',
+        apple_music_link: 'Enlace de Apple Music',
+        languageMenu: {
+          english: 'EN',
+          spanish: 'ES',
+          language: 'ES'
+        },
+        aboutSection: {
+          title: 'Acerca de',
+          text: 'ATKL Records, fundada por <a href="https://soundcloud.com/rodro_music" target="_blank" rel="noopener noreferrer" class="hover:text-red-500">RODRO</a> en diciembre de 2022 en Chile, se caracteriza por su distintiva fusión de géneros como hardcore techno, industrial y schranz, reconocidos por sus paisajes sonoros pesados, energéticos y dinámicos. El primer lanzamiento del sello, "<a href="https://open.spotify.com/album/unknown-va-i" target="_blank" rel="noopener noreferrer" class="hover:text-red-500">Unknown VA I</a>", un compilado que incluye a 10 artistas emergentes de Chile, Colombia y España, se lanzó en marzo de 2023, marcando el inicio de su trayectoria. Desde entonces, ATKL Records ha crecido hasta exhibir más de 12 lanzamientos diversos y cuenta con dos podcasts populares en SoundCloud, destacando tanto talentos establecidos como prometedores en la escena global de la música electrónica. A mediados de 2023, el sello expandió su influencia al organizar eventos y fiestas enérgicas, consolidando aún más su rol no solo como sello discográfico sino también como un vibrante punto de encuentro comunitario donde DJs locales y entusiastas de la música convergen.'
+        },
+        contactSection: {
+          title: 'Contáctanos'
+        },
+        footer: {
+          label: 'Mapa del sitio',
+          aboutUs: 'Sobre Nosotros',
+          artists: 'Artistas',
+          releases: 'Lanzamientos',
+          sendDemo: 'Enviar Demo',
+          instagram: 'Instagram',
+          soundcloud: 'SoundCloud',
+          beatport: 'Beatport',
+          bandcamp: 'Bandcamp',
+          booking: 'Booking',
+          mastering: 'Mastering',
+          freeReleases: 'Lanzamientos Gratuitos',
+          affiliateProgram: 'Programa de Afiliados',
+          events: 'Eventos',
+          discography: 'Discografía',
+          merchandising: 'Merchandising',
+          samplePacks: 'Paquetes de Muestras',
+          rightsReserved: 'Todos los Derechos Reservados'
+        },
+        login: {
           email: 'Correo electrónico',
-          description: 'Descripción (Opcional)',
-          submit: 'Enviar',
-          subscribe_message: 'Suscríbete para recibir las últimas actualizaciones gratis por correo electrónico',
-          description_optional: 'Descripción (Opcional)',
-          form_sent_success: 'Formulario enviado con éxito',
-          send_demo: 'Enviar Demo',
-          upload_cover_image: 'Subir Portada',
-          upload_profile_image: 'Subir Imagen de Perfil',
-          artistName: 'Nombre del Artista',
-          image: 'Imagen',
-          bandcamp_link: 'Enlace de Bandcamp',
-          facebook_link: 'Enlace de Facebook',
-          instagram_link: 'Enlace de Instagram',
-          soundcloud_link: 'Enlace de SoundCloud',
-          twitter_link: 'Enlace de Twitter',
-          youtube_link: 'Enlace de YouTube',
-          spotify_link: 'Enlace de Spotify',
-          beatport_link: 'Enlace de Beatport',
-          apple_music_link: 'Enlace de Apple Music',
-          languageMenu: {
-            english: 'EN',
-            spanish: 'ES',
-            language: 'ES'
+          password: 'Contraseña',
+          login: 'Iniciar sesión',
+          failed: 'Inicio de sesión fallido. Por favor, verifica tus credenciales y vuelve a intentarlo.',
+          welcome: 'Bienvenido a ATKL Records',
+        },
+        addArtist: {
+          title: 'Agregar Artista',
+          username: 'Nombre de Usuario',
+          password: 'Contraseña',
+          bio: 'Bio',
+          role: {
+            role: 'Rol',
+            dj: 'DJ',
+            producer: 'Productor'
           },
-          aboutSection: {
-            title: 'Acerca de',
-            text: 'ATKL Records, fundada por <a href="https://soundcloud.com/rodro_music" target="_blank" rel="noopener noreferrer" class="hover:text-red-500">RODRO</a> en diciembre de 2022 en Chile, se caracteriza por su distintiva fusión de géneros como hardcore techno, industrial y schranz, reconocidos por sus paisajes sonoros pesados, energéticos y dinámicos. El primer lanzamiento del sello, "<a href="https://open.spotify.com/album/unknown-va-i" target="_blank" rel="noopener noreferrer" class="hover:text-red-500">Unknown VA I</a>", un compilado que incluye a 10 artistas emergentes de Chile, Colombia y España, se lanzó en marzo de 2023, marcando el inicio de su trayectoria. Desde entonces, ATKL Records ha crecido hasta exhibir más de 12 lanzamientos diversos y cuenta con dos podcasts populares en SoundCloud, destacando tanto talentos establecidos como prometedores en la escena global de la música electrónica. A mediados de 2023, el sello expandió su influencia al organizar eventos y fiestas enérgicas, consolidando aún más su rol no solo como sello discográfico sino también como un vibrante punto de encuentro comunitario donde DJs locales y entusiastas de la música convergen.'
+          selectRole: 'Seleccionar Rol',
+          validation: {
+            artistNameRequired: 'El nombre del artista es requerido',
+            usernameRequired: 'El nombre de usuario es requerido',
+            emailRequired: 'El correo electrónico es requerido',
+            invalidEmail: 'Correo electrónico inválido',
+            passwordRequired: 'La contraseña es requerida',
+            passwordMin: 'La contraseña debe tener al menos 6 caracteres',
+            roleRequired: 'Se requiere al menos un rol',
+
           },
-          contactSection: {
-            title: 'Contáctanos'
-          },
-          footer: {
-            label: 'Mapa del sitio',
-            aboutUs: 'Sobre Nosotros',
-            artists: 'Artistas',
-            releases: 'Lanzamientos',
-            sendDemo: 'Enviar Demo',
-            instagram: 'Instagram',
-            soundcloud: 'SoundCloud',
-            beatport: 'Beatport',
-            bandcamp: 'Bandcamp',
-            booking: 'Booking',
-            mastering: 'Mastering',
-            freeReleases: 'Lanzamientos Gratuitos',
-            affiliateProgram: 'Programa de Afiliados',
-            events: 'Eventos',
-            discography: 'Discografía',
-            merchandising: 'Merchandising',
-            samplePacks: 'Paquetes de Muestras',
-            rightsReserved: 'Todos los Derechos Reservados'
-          },
-          login: {
-            email: 'Correo electrónico',
-            password: 'Contraseña',
-            login: 'Iniciar sesión',
-            failed: 'Inicio de sesión fallido. Por favor, verifica tus credenciales y vuelve a intentarlo.',
-            welcome: 'Bienvenido a ATKL Records',
-          },
-          addArtist: {
-            title: 'Agregar Artista',
+          label: {
+            artist_name: 'Nombre del Artista',
             username: 'Nombre de Usuario',
             password: 'Contraseña',
+            email: 'Correo electrónico',
             bio: 'Bio',
-            role: {
-              role: 'Rol',
-              dj: 'DJ',
-              producer: 'Productor'
-            },
-            selectRole: 'Seleccionar Rol',
-            validation: {
-              artistNameRequired: 'El nombre del artista es requerido',
-              usernameRequired: 'El nombre de usuario es requerido',
-              emailRequired: 'El correo electrónico es requerido',
-              invalidEmail: 'Correo electrónico inválido',
-              passwordRequired: 'La contraseña es requerida',
-              passwordMin: 'La contraseña debe tener al menos 6 caracteres',
-              roleRequired: 'Se requiere al menos un rol',
-
-            },
-            label: {
-              artist_name: 'Nombre del Artista',
-              username: 'Nombre de Usuario',
-              password: 'Contraseña',
-              email: 'Correo electrónico',
-              bio: 'Bio',
-              role: 'Rol',
-              dj: 'DJ',
-              producer: 'Productor'
-            }
-          },
-
-        }
+            role: 'Rol',
+            dj: 'DJ',
+            producer: 'Productor'
+          }
+        },
 
       }
-    },
-    lng: 'en', // idioma por defecto
-    fallbackLng: 'en',
-    interpolation: {
-      escapeValue: false // React ya se encarga de esto
+
     }
-  })
+  },
+  lng: 'en', // idioma por defecto
+  fallbackLng: 'en',
+  interpolation: {
+    escapeValue: false // React ya se encarga de esto
+  }
+})
 
 export default i18n
